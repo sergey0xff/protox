eb939cdb717f8551722f86f5f459f3d37eca9a3c
Sergey Tikhonov <srg.tikhonov@gmail.com>
2019-12-23 00:33:15 +0300

    Added code generator maps support without enums

--- app/protobuf/hello_protox_pb.py
+++ app/protobuf/hello_protox_pb.py
@@ -1,3 +1,4 @@
+import typing
 from enum import IntEnum
 
 import protox
@@ -5,12 +6,21 @@ import protox
 
 class User(protox.Message):
     class Type(IntEnum):
+        UNDEFINED = 0
         MERE_MORTAL = 1
         ADMIN = 2
 
     id: int
     name: str
     type: 'User.Type'
+    props: typing.Dict[str, 'User.Type']
+    ok: typing.Optional[int]
+    fail: typing.Optional[int]
+
+    result = protox.one_of(
+        'ok',
+        'fail',
+    )
 
     def __init__(
         self,
@@ -18,11 +28,17 @@ class User(protox.Message):
         id: int,
         name: str,
         type: 'User.Type',
+        props: typing.Dict[str, 'User.Type'] = None,
+        ok: typing.Optional[int] = None,
+        fail: typing.Optional[int] = None,
     ):
         super().__init__(
             id=id,
             name=name,
             type=type,
+            props=props,
+            ok=ok,
+            fail=fail,
         )
 
 
@@ -36,5 +52,14 @@ protox.define_fields(
     ),
     type=protox.EnumField(
         number=3, py_enum=User.Type, default=User.Type.MERE_MORTAL, required=True
+    ),
+    props=protox.MapField(
+        number=4, key=protox.String, value=protox.EnumField
+    ),
+    ok=protox.Int32(
+        number=10, required=False
+    ),
+    fail=protox.Int32(
+        number=11, required=False
     ),
 )
