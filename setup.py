2661bfb4868283cc0316ef3c5cb0c4b6bdaa4782
Sergey Tikhonov <srg.tikhonov@gmail.com>
2020-04-19 21:31:34 +0300

    Incremented version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='protox',
-    version='0.1.6',
+    version='0.1.7',
     description='Protobuf for humans',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
