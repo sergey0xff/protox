35dd612fdabc8d706a74fd5037bea959672944b1
Sergey Tikhonov <srg.tikhonov@gmail.com>
2020-05-04 17:35:56 +0300

    Improved code generator tests

--- code_gen_tests.sh
+++ code_gen_tests.sh
@@ -2,14 +2,21 @@
 
 tmp_dir="$(mktemp -d -t proto-XXXXXXXXXX)"
 
+pip3 install .
+echo '+ Protox installed'
+
 protoc \
-  --proto_path=compiler_tests \
+  --proto_path=code_gen_tests \
   --protox_out=$tmp_dir \
   --protox_opt="--with-dependencies" \
-  compiler_tests/*.proto
+  code_gen_tests/*.proto
+
+echo '+ Protobuf messages compiled'
 
 for filename in $tmp_dir/*.py; do
-  python3.7 $filename
+  python3 $filename
 done
 
 rm -rf $tmp_dir
+
+echo '+ All tests passed'
